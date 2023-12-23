b33788c5c8b1493b0930e0fc1f7cc7ca5d7836d9
DML <djamel.dyne.gaming@gmail.com>
2023-12-23 18:17:19 +0100

    code push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'link': ['Zurich'],
+    },
     extend: {}
+  },
+  variants: {
+    extend: {
+      visibility: ['responsive']
+    }
   },
   plugins: []
 };
