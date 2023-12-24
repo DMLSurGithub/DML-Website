2a1572ac7df8fe989925b58ad8d0eaf7f300237c
DML <djamel.dyne.gaming@gmail.com>
2023-12-24 23:33:52 +0100

    code push

--- src/lib/components/qui/histoire/Histoire.svelte
+++ src/lib/components/qui/histoire/Histoire.svelte
@@ -1,38 +1,38 @@
 <script>
   import informatique from '../../../images/IMG/histoire/dev.jpg';
-  import scolaire from '../../../images/IMG/histoire/etudes.jpg';
-  import web from '../../../images/IMG/histoire/web.png';
+  import scolaire from '../../../images/IMG/histoire/etudes.png';
+  import web from '../../../images/IMG/histoire/web.jpg';
 </script>
 
 <div class="pt-[50px] pb-[50px] flex flex-col items-center justify-center bg-[#f4f3fc] dark:bg-[#0C0C12]" id="quest-container">
 
-  <div class="flex items-center justify-center flex-row mb-[60px]" id="informatique">
-    <div class="flex items-center justify-center" id="image-informatique">
-      <img class="w-[600px] rounded-[10px]" src={informatique} alt="Informatique">
-    </div>
+  <div class="flex items-center justify-center flex-row mb-[60px] mr-[100px]" id="informatique">
     <div class="bg-[#d4d5e2] dark:bg-[#14141E] w-[600px] rounded-[10px] flex flex-col items-center justify-center p-[20px] mx-[50px]" id="texte-informatique">
       <h1 class="font-bold pb-4 text-[30px] text-[#4e59d8] dark:text-[#5865f2] font-family: 'Outfit', sans-serif">Pourquoi l'informatique ?</h1>
       <p class="text-[15px] text-black dark:text-[#ffffff] font-family: 'Outfit', sans-serif">Depuis mon plus jeune âge, j’ai toujours été fasciné par les ordinateurs. Je me souviens avoir été émerveillé par tout ce que l’on pouvait créer. Cette fascination précoce a grandement influencé mon parcours et a orienté mes intérêts vers l’informatique.Mon rêve d’enfant était de devenir un hacker, un expert capable de naviguer dans les méandres des systèmes informatiques les plus sécurisés. Cependant, j’ai rapidement compris que le piratage d’entreprises était illégal et potentiellement nuisible. Au lieu de cela, mon intérêt s’est porté sur le fonctionnement interne des logiciels.<br/><br/>En ce qui concerne mon avenir, j’espère un jour pouvoir créer un jeu qui me ressemble. Je souhaite également intégrer une école d’informatique qui me poussera à pleine capacité dans la recherche et la création en informatique. Je suis impatient de voir où cette passion pour l’informatique me mènera.<br/><br/>Je souhaite m’orienter vers la cybersécurité, un domaine qui est non seulement passionnant, mais aussi extrêmement pertinent dans le monde numérique d’aujourd’hui. Cela semble être une évolution naturelle de mon rêve d’enfance de devenir un hacker, mais dans un contexte légal et éthique.<br/>Dans le domaine de la cybersécurité, je suis particulièrement attiré par les métiers de Hacker éthique et d’Architecte en cybersécurité. Ces rôles semblent correspondre à mes intérêts et à mes aspirations.<br/>En ce qui concerne la combinaison de mon intérêt pour la création de jeux avec mon aspiration à travailler dans la cybersécurité, j’envisage de faire de la cybersécurité mon métier principal. Mon jeu pourrait avoir un aspect lié au hacking, ce qui serait une excellente façon de combiner mes deux passions.<br/><br/>Quant aux défis spécifiques que j’espère relever dans le domaine de la cybersécurité, je souhaite être capable de hacker tout ce qu’on me demande et de ne jamais rester bloqué sur une protection. C’est un objectif ambitieux, mais je suis prêt à travailler dur pour l’atteindre.</p>
     </div>
+    <div class="flex items-center justify-center" id="image-informatique">
+      <img class="w-[600px] rounded-[10px]" src={informatique} alt="Informatique">
+    </div>
   </div>
 
-  <div class="flex items-center justify-center flex-row mb-[60px] mr-[100px]" id="scolaire">
+  <div class="flex items-center justify-center flex-row mb-[60px]" id="scolaire">
+    <div class="flex items-center justify-center" id="image-scolaire">
+      <img class="w-[600px] rounded-[10px]" src={scolaire} alt="Scolaire">
+    </div>
     <div class="bg-[#d4d5e2] dark:bg-[#14141E] w-[600px] rounded-[10px] flex flex-col items-center justify-center p-[20px] mx-[50px]" id="texte-scolaire">
       <h1 class="font-bold pb-4 text-[30px] text-[#4e59d8] dark:text-[#5865f2] font-family: 'Outfit', sans-serif">Mon parcours :</h1>
       <p class="text-[15px] text-black dark:text-[#ffffff] font-family: 'Outfit', sans-serif">Mon parcours académique et professionnel est le reflet de ma passion pour l’informatique. J’ai commencé mon voyage au collège de 2017 à 2021, où j’ai obtenu plusieurs diplômes, dont l’ASSR 1, l’ASSR 2, le PSC 1 et le Brevet des collèges avec mention bien.<br/>De 2021 à 2024, j’ai poursuivi mes études au lycée, où j’ai obtenu mon BAC Général. Ces années ont été une période de croissance et d’apprentissage, me permettant d’acquérir une solide base académique.<br/><br/>Mon objectif actuel est d’intégrer une école d’informatique. Je suis particulièrement intéressé par les cursus en Cybersécurité ou en Systèmes, Réseaux et Cloud Computing. Selon le cursus que je choisirai, j’aspire à devenir Hacker éthique ou Architecte en cybersécurité si je prends le cursus en Cybersécurité, ou Architecte Réseaux si je prends le cursus en Systèmes, Réseaux et Cloud Computing.<br/>En parallèle de mon parcours j'ai appris à développer des applications web en utilisant Svelte, TailwindCSS, HTML, CSS, JavaScript ou encore Next.<br/><br/>En somme, mon parcours est celui d’un passionné d’informatique, toujours prêt à apprendre et à relever de nouveaux défis. Je suis impatient de voir où cette passion me mènera.</p>
     </div>
-    <div class="flex items-center justify-center" id="image-scolaire">
-      <img class="w-[600px] rounded-[10px]" src={scolaire} alt="Scolaire">
-    </div>
   </div>
 
-  <div class="flex items-center justify-center flex-row" id="web">
-    <div class="flex items-center justify-center" id="image-web">
-      <img class="w-[600px] rounded-[10px]" src={web} alt="Web">
-    </div>
+  <div class="flex items-center justify-center flex-row mr-[100px]" id="web">
     <div class="bg-[#d4d5e2] dark:bg-[#14141E] w-[600px] rounded-[10px] flex flex-col items-center justify-center p-[20px] mx-[50px]" id="texte-web">
       <h1 class="font-bold pb-4 text-[30px] text-[#4e59d8] dark:text-[#5865f2] font-family: 'Outfit', sans-serif">Le développement web :</h1>
       <p class="text-[15px] text-black dark:text-[#ffffff] font-family: 'Outfit', sans-serif">On m’a conseillé d’apprendre le développement web car c’est un domaine intéressant et enrichissant pour toute personne travaillant dans l’informatique. J’ai suivi ce conseil et j’ai réalisé plusieurs sites web jusqu’à présent, que vous pouvez retrouver sur mon compte GitHub.<br/><br/>Le développement web est un domaine passionnant pour plusieurs raisons. Il offre une plateforme pour exprimer ma créativité, que ce soit en termes de design, d’interactivité ou de fonctionnalités, les possibilités sont infinies. Chaque projet web est un nouveau défi, et trouver des solutions aux problèmes rencontrés lors du développement d’un site web est une expérience enrichissante.<br/>Le domaine du web évolue constamment. Il y a toujours quelque chose de nouveau à apprendre, que ce soit un nouveau framework, une nouvelle bibliothèque ou une nouvelle approche de conception. Un site web peut atteindre un public mondial, et il est gratifiant de savoir que le travail que je fais peut être utilisé et apprécié par des personnes du monde entier.<br/>Avec les nombreuses technologies et langages disponibles, le développement web offre une grande flexibilité. Je peux choisir les outils qui correspondent le mieux à mes besoins et à mes préférences. Ces qualités font du développement web un domaine passionnant et gratifiant. Je suis impatient de continuer à apprendre et à créer dans ce domaine.</p>
+    </div>
+    <div class="flex items-center justify-center" id="image-web">
+      <img class="w-[600px] rounded-[10px]" src={web} alt="Web">
     </div>
   </div>
 </div>
